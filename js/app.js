9f846e2617226fe148a96a55514933aca6572abe
jdalton308 <jdalton308@yahoo.com>
2015-11-16 02:23:45 -0800

    Created game logic, and some more primative views

--- js/app.js
+++ js/app.js
@@ -39330,48 +39330,150 @@ app.factory('Images', [function(){
 
 app.controller('mainCtrl', ['$scope', '$location', function($scope, $location) {
 
-	$scope.leaveIntro = function(){
+	$scope.leaveIntro = function() {
 		$location.url('/how-to-play');
 	}
 
-	$scope.startGame = function(){
+	$scope.startGame = function() {
 		$location.url('/game');
+	}
+
+	$scope.leaveWin = function() {
+		$location.url('/thanks');
 	}
 
 }]);
 
 app.controller('gameCtrl', ['$scope', '$location', 'Images', function($scope, $location, Images) {
 
-	// Initialize image
+
+	//// Variables ////
 
 	var round = 1;
 	var totalRounds = 3;
+	var timeRef = {
+		1: 45,
+		2: 45,
+		3: 30
+	};
 	var images;
 	$scope.game = {};
+	$scope.game.currentImg;
 
-	var getRoundImages = function(roundNum) {
-		return Images[roundNum];
+
+	//// Initialize Image ////
+	// To progress the image, just update the var round, then call setImage().
+
+	var getRoundImages = function() {
+		return Images[round];
 	}
 	var pickImage = function(imageArray) {
 		var range = imageArray.length;
 		var index = Math.floor(Math.random() * range);
+		console.log('Image index: '+ index);
 
 		return imageArray[index];
 		// returns object with hotspot coordinates
 	}
 
-	var initImage = function() {
-		images = getRoundImages(round);
+	var setImage = function() {
+		images = getRoundImages();
 		$scope.game.currentImg =  pickImage(images);
 	}
 
-	initImage();
+	setImage();
 
 
-	// Gameplay
+	//// Timer ////
+
+	var timerRef;
+	var interval = 500;
+	var totalTime;
+	$scope.game.currentTime;
+	$scope.game.timerWidth;
+
+	var setRoundTime = function() {
+		totalTime = timeRef[round] * 1000;
+		console.log("Total Time: "+ totalTime);
+		$scope.game.currentTime = totalTime;
+	}
+	$scope.addPenalty = function() {
+		console.log('Penalty occured');
+		$scope.game.currentTime -= 5000;
+	}
+	var updateTimer = function() {
+		$scope.game.currentTime -= interval;
+		var currentRatio = $scope.game.currentTime/totalTime * 100;
+
+		if (currentRatio > 0) {
+			$scope.game.timerWidth = currentRatio + '%';
+		} else {
+			$scope.game.timerWidth = 0;
+			stopTimer();
+			$scope.lostImage();
+		}
+		$scope.$apply();
+	}
+	var startTimer = function() {
+		setRoundTime();
+		timerRef = setInterval(updateTimer, interval);
+		console.log('Timer started');
+	}
+	var stopTimer = function() {
+		clearInterval(timerRef);
+		console.log('Timer stopped');
+	}
+
+	startTimer();
 
 
+	//// Gameplay ////
 
-	// Game Progression
+	$scope.game.selectedSpots = {};
+
+	$scope.foundOne = function(index) {
+		$scope.game.selectedSpots[index] = true;
+		console.log('Selected Spots:');
+		console.log($scope.game.selectedSpots);
+
+		var selectedLength = Object.keys($scope.game.selectedSpots).length;
+		var totalLength = Object.keys($scope.game.currentImg).length;
+
+		if (selectedLength == totalLength) {
+			console.log('FOUND THEM ALL');
+			$scope.completedImage();
+		}
+	}
+	$scope.completedImage = function() {
+		stopTimer();
+		$scope.game.message = {
+			prompt: "YOU DID IT!",
+			button: "NEXT"
+		}
+	}
+	$scope.lostImage = function() {
+		$scope.game.message = {
+			prompt: "YOU LOST",
+			button: "NEXT"
+		}
+	}
+	$scope.startNextImage = function() {
+		if (round != totalRounds) {
+			round += 1;
+			$scope.game.selectedSpots = {};
+			setImage();
+			startTimer();
+			$scope.game.message = null;
+		} else {
+			$location.url('/win');
+		}
+	}
+	$scope.game.giveUp = function() {
+		stopTimer();
+		$scope.lostImage();
+	}
+
+
+	//// Game Progression ////
 
 }]);
